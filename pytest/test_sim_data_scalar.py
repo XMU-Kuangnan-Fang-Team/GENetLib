64445e6095019e7973d4e287f2c59cc7f075cf71
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-26 21:44:42 +0800

    Update and rename test_all.py to test_sim_data_scalar.py

---
+++ pytest/test_sim_data_scalar.py
@@ -0,0 +1,6 @@
+from GENetLib.sim_data_scalar import sim_data_scalar
+
+def test_sim_data_scalar():
+    scalar_survival_linear = sim_data_scalar(rho_G=0.25, rho_E=0.3, dim_G=500, dim_E=5, n=1500, dim_E_Sparse=2, ytype='Survival', n_inter=30)
+    assert scalar_survival_linear[0] is not None
+    assert scalar_survival_linear[1] is not None
