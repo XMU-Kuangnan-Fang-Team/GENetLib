3ed31953ce77f51fffc7e9c7bd46821a9e3ddf31
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-26 21:55:08 +0800

    Update test_sim_data_scalar.py

--- pytest/test_sim_data_scalar.py
+++ pytest/test_sim_data_scalar.py
@@ -4,3 +4,4 @@ def test_sim_data_scalar():
     scalar_survival_linear = sim_data_scalar(rho_G=0.25, rho_E=0.3, dim_G=500, dim_E=5, n=1500, dim_E_Sparse=2, ytype='Survival', n_inter=30)
     assert scalar_survival_linear[0] is not None
     assert scalar_survival_linear[1] is not None
+test_sim_data_scalar
