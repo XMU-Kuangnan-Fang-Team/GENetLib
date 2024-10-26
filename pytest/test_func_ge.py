7243837bfb30754964d3342fb6a0e72344ed64d9
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-27 00:37:28 +0800

    Update test_func_ge.py

--- pytest/test_func_ge.py
+++ pytest/test_func_ge.py
@@ -7,4 +7,4 @@ def test_grid_func_ge():
                         func_continuous['X'], 'Continuous', 'Bspline', num_hidden_layers=2, nodes_hidden_layer=[20,5], 
                         Learning_Rate2=0.035, L2=0.01, Learning_Rate1=0.02, L=0.01, Num_Epochs=1, 
                         nbasis1=5, params1=4, Bsplines=5, norder1=4, model=None, split_type=1, ratio=[3, 1, 1], plot_res=False)
-
+  assert func_ge_res is not None
