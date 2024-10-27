e6203119e07dc6df43cbb55fc4174f792f82349c
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-27 20:20:13 +0800

    Update test_func_ge.py

--- pytest/test_func_ge.py
+++ pytest/test_func_ge.py
@@ -9,7 +9,7 @@ def test_func_ge():
   func_survival = sim_data_func(n=500, m=30, ytype='Survival', seed=123)
   func_ge_res_1 = func_ge(func_continuous['y'], func_continuous['z'], func_continuous['location'], func_continuous['X'], 
                           'Continuous', 'Bspline', num_hidden_layers=2, nodes_hidden_layer=[100,10], Learning_Rate2=0.035, 
-                          L2=0.01, Learning_Rate1=0.02, L=0.01, Num_Epochs=50, nbasis1=5, params1=4, Bsplines=5, norder1=4, 
+                          L2=0.01, Learning_Rate1=0.02, L=0.01, Num_Epochs=50, nbasis1=5, params1=4, t=0.01, Bsplines=5, norder1=4, 
                           model=None, split_type=1, ratio=[3, 1, 1], plot_res=False)
   func_ge_res_2 = func_ge(func_binary['y'], func_binary['z'], func_binary['location'], func_binary['X'], 
                           'Binary', 'Fourier', num_hidden_layers=2, nodes_hidden_layer=[100,10], Learning_Rate2=0.035, 
@@ -19,11 +19,6 @@ def test_func_ge():
                           'Survival', 'Power', num_hidden_layers=2, nodes_hidden_layer=[100,10], Learning_Rate2=0.035, 
                           L2=0.01, Learning_Rate1=0.02, L=0.01, Num_Epochs=50, nbasis1=5, params1=[-1,-0.5,0,0.5,1], Bsplines=5, norder1=4, 
                           model=None, split_type=0, ratio=[7, 3], plot_res=True, plot_beta = False) 
-  func_ge_res_4 = func_ge(func_continuous['y'], func_continuous['z'], func_continuous['location'], func_continuous['X'], 
-                          'Continuous', 'Exponential', num_hidden_layers=2, nodes_hidden_layer=[100,10], Learning_Rate2=0.035, 
-                          L2=0.01, Learning_Rate1=0.02, L=0.01, Num_Epochs=50, nbasis1=5, params1=4, t=0.01, Bsplines=5, norder1=4, 
-                          model=None, split_type=0, ratio=[7, 3], plot_res=True)  
   assert func_ge_res_1 is not None
   assert func_ge_res_2 is not None
   assert func_ge_res_3 is not None
-  assert func_ge_res_4 is not None
