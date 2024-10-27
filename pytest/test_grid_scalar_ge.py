8d24e239f6c3126a9c1297fdbc495a01a207705a
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-27 23:29:32 +0800

    Update test_grid_scalar_ge.py

--- pytest/test_grid_scalar_ge.py
+++ pytest/test_grid_scalar_ge.py
@@ -9,17 +9,17 @@ def test_grid_scalar_ge():
   scalar_survival = sim_data_scalar(rho_G=0.25, rho_E=0.3, dim_G=500, dim_E=5, n=500, dim_E_Sparse=2, ytype='Survival', n_inter=10, linear = False)
   grid_scalar_ge_res_1 = grid_scalar_ge(data=scalar_continuous[0], ytype='Continuous', dim_G=500, dim_E=5, haveGE=True, 
                                         num_hidden_layers=2, nodes_hidden_layer=[1000,100], 
-                                        Learning_Rate2=[0.035,0.045], L2=[0.01], Learning_Rate1=[0.04, 0.05, 0.06], L=[0.08, 0.09], 
+                                        Learning_Rate2=[0.035,0.045], L2=[0.1], Learning_Rate1=[0.04, 0.05, 0.06], L=[0.08, 0.09], 
                                         Num_Epochs=50, t = 0.01, model = None, split_type = 0, ratio = [7, 3], 
                                         important_feature = True, plot = True, model_reg = None, isfunc = False)
   grid_scalar_ge_res_2 = grid_scalar_ge(data=scalar_binary[0], ytype='Binary', dim_G=500, dim_E=5, haveGE=False, 
                                         num_hidden_layers=2, nodes_hidden_layer=[1000,100], 
-                                        Learning_Rate2=[0.035,0.045], L2=[0.01], Learning_Rate1=[0.04, 0.05, 0.06], L=[0.08, 0.09], 
+                                        Learning_Rate2=[0.035,0.045], L2=[0.1], Learning_Rate1=[0.04, 0.05, 0.06], L=[0.08, 0.09], 
                                         Num_Epochs=50, t = 0.01, model = None, split_type = 1, ratio = [7, 2, 1], 
                                         important_feature = False, plot = False, model_reg = None, isfunc = False)
   grid_scalar_ge_res_3 = grid_scalar_ge(data=scalar_survival[0], ytype='Survival', dim_G=500, dim_E=5, haveGE=False, 
                                         num_hidden_layers=2, nodes_hidden_layer=[1000,100], 
-                                        Learning_Rate2=[0.035,0.045], L2=[0.01], Learning_Rate1=[0.04, 0.05, 0.06], L=[0.08, 0.09], 
+                                        Learning_Rate2=[0.035,0.045], L2=[0.1], Learning_Rate1=[0.04, 0.05, 0.06], L=[0.08, 0.09], 
                                         Num_Epochs=50, t = 0.01, model = None, split_type = 1, ratio = [7, 2, 1], 
                                         important_feature = False, plot = False, model_reg = None, isfunc = False)
   assert scalar_ge_res_1 is not None
