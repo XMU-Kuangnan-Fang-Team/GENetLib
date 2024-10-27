75778f47a6d5b5c0ad9931ec43d3e6463eb83e20
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-27 08:54:23 +0800

    Update test_func_ge.py

--- pytest/test_func_ge.py
+++ pytest/test_func_ge.py
@@ -1,10 +1,12 @@
 from GENetLib.sim_data_func import sim_data_func
 from GENetLib.func_ge import func_ge
+import matplotlib
+matplotlib.use('Agg')
 
 def test_func_ge():
   func_continuous = sim_data_func(n=50, m=30, ytype='Continuous', seed=123)
   func_ge_res = func_ge(func_continuous['y'], func_continuous['z'], func_continuous['location'], func_continuous['X'], 
                         'Continuous', 'Bspline', num_hidden_layers=1, nodes_hidden_layer=[2], Learning_Rate2=0.035, 
                         L2=0.01, Learning_Rate1=0.02, L=0.01, Num_Epochs=50, nbasis1=5, params1=4, Bsplines=5, norder1=4, 
-                        model=None, split_type=1, ratio=[3, 1, 1], plot_res=True)
+                        model=None, split_type=1, ratio=[3, 1, 1], plot_res=False)
   assert func_ge_res is not None
