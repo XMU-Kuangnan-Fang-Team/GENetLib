876b265089359e59bf1a43b605412aef7b7d2e87
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-28 02:06:34 +0800

    Update test_plot.py

--- pytest/test_plot.py
+++ pytest/test_plot.py
@@ -8,3 +8,4 @@ from GENetLib.basis_fd import basis_fd
 
 def test_plot_fd():
     plot_fd(fd_chk(basis_fd(btype="fourier", rangeval=[0, 1], nbasis=4, params=[2 * np.pi]))[1])
+    assert plt.gcf() is not None
