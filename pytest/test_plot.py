38d415020f4e00ce0d003f50a18036d2f4c3de3c
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-28 02:34:19 +0800

    Update test_plot.py

--- pytest/test_plot.py
+++ pytest/test_plot.py
@@ -5,8 +5,17 @@ matplotlib.use('Agg')
 from GENetLib.plot_fd import plot_fd
 from GENetLib.fd_chk import fd_chk
 from GENetLib.basis_fd import basis_fd
+from GENetLib.plot_rawdata import plot_rawdata
+from GENetLib.sim_data_func import sim_data_func
 
 
 def test_plot_fd():
     plot_fd(fd_chk(basis_fd(btype="fourier", rangeval=[0, 1], nbasis=4, params=[2 * np.pi]))[1])
     assert plt.gcf() is not None
+
+def test_plot_rawdata():
+    func_continuous = sim_data_func(n=1500, m=30, ytype='Continuous', seed=123)
+    plot_rawdata(func_continuous['location'], func_continuous['X'], color = None, pch = 4, cex = 0.9):
+    assert plt.gcf() is not None
+
+    
