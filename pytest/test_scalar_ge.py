d30af75eaa1d7df648af935305f7bab9cab3c0ec
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-28 11:55:53 +0800

    Update test_scalar_ge.py

--- pytest/test_scalar_ge.py
+++ pytest/test_scalar_ge.py
@@ -7,17 +7,17 @@ def test_func_ge():
   scalar_continuous = sim_data_scalar(rho_G=0.25, rho_E=0.3, dim_G=500, dim_E=5, n=500, dim_E_Sparse=2, ytype='Continuous', n_inter=30)
   scalar_binary = sim_data_scalar(rho_G=0.25, rho_E=0.3, dim_G=500, dim_E=5, n=500, dim_E_Sparse=2, ytype='Binary', n_inter=10)
   scalar_survival = sim_data_scalar(rho_G=0.25, rho_E=0.3, dim_G=500, dim_E=5, n=500, dim_E_Sparse=2, ytype='Survival', n_inter=10, linear = False)
-  scalar_ge_res_1 = scalar_ge(data=scalar_continuous[0], ytype='Continuous', dim_G=500, dim_E=5, haveGE=True, 
+  scalar_ge_res_1 = scalar_ge(data=scalar_continuous['data'], ytype='Continuous', dim_G=500, dim_E=5, haveGE=True, 
                               num_hidden_layers=2, nodes_hidden_layer=[1000,100], 
                               Learning_Rate2=0.035, L2=0.01, Learning_Rate1=0.06, L=0.09, 
                               Num_Epochs=50, t = 0.01, model = None, split_type = 0, ratio = [7, 3], 
                               important_feature = True, plot = True, model_reg = None, isfunc = False)
-  scalar_ge_res_2 = scalar_ge(data=scalar_binary[0], ytype='Binary', dim_G=500, dim_E=5, haveGE=True, 
+  scalar_ge_res_2 = scalar_ge(data=scalar_binary['data'], ytype='Binary', dim_G=500, dim_E=5, haveGE=True, 
                               num_hidden_layers=2, nodes_hidden_layer=[1000,100], 
                               Learning_Rate2=0.035, L2=0.01, Learning_Rate1=0.06, L=0.09, 
                               Num_Epochs=50, t = 0.01, model = None, split_type = 1, ratio = [7, 2, 1], 
                               important_feature = True, plot = False, model_reg = None, isfunc = False)
-  scalar_ge_res_3 = scalar_ge(data=scalar_survival[0], ytype='Survival', dim_G=500, dim_E=5, haveGE=False, 
+  scalar_ge_res_3 = scalar_ge(data=scalar_survival['data'], ytype='Survival', dim_G=500, dim_E=5, haveGE=False, 
                               num_hidden_layers=2, nodes_hidden_layer=[1000,100], 
                               Learning_Rate2=0.035, L2=0.01, Learning_Rate1=0.06, L=0.09, 
                               Num_Epochs=50, t = 0.01, model = None, split_type = 1, ratio = [7, 2, 1], 
