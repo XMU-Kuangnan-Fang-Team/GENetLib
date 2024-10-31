53b27e8858981ee5be7dfc8298b516d79c829a18
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-31 17:28:25 +0000

    init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_documatt_theme"
+html_static_path = ['_static']
 
-html_logo = '_static/logo.jpg'
-html_favicon = '_static/logo.jpg'
+html_logo = '_static/framework.png'
+html_favicon = '_static/framework.png'
