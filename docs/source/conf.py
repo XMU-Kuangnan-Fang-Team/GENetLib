bd5812393f989db084f9b810f829b0cfc088fe31
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-10-31 17:17:17 +0000

    init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,5 +28,5 @@ exclude_patterns = []
 html_theme = "sphinx_documatt_theme"
 html_static_path = ['_static']
 
-html_logo = 'logo.jpg'
-html_favicon = 'logo.jpg'
+html_logo = '_static/logo.jpg'
+html_favicon = '_static/logo.jpg'
