034cf2a23b6189c486b0e20216048895a7f7bc12
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-11-07 08:58:48 +0800

    Update plot_rawdata.py

--- GENetLib/plot_rawdata.py
+++ GENetLib/plot_rawdata.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 '''Plot functions fitting from densely measured observations'''
 
-def plot_rawdata(location, X, color = None, pch = 4, cex = 0.9):
+def plot_rawdata(location, X, color=None, pch=4, cex=0.9, show_legend=True):
     n, m = X.shape
     type_ = 'o'
     truelengths = np.sum(~np.isnan(X))
@@ -13,8 +13,17 @@ def plot_rawdata(location, X, color = None, pch = 4, cex = 0.9):
             plt.plot(location, X.T, marker=type_, markersize=pch, label='X')
         else:
             plt.plot(location, X.T, marker=type_, markersize=pch, color=color, label='X')
+    else:
+        location_list = [location[i][~np.isnan(X[i, :])] for i in range(n)]
+        X_list = [X[i, ~np.isnan(X[i, :])] for i in range(n)]
+        if color is None:
+            for i in range(n):
+                plt.plot(location_list[i], X_list[i], marker=type_, markersize=pch, label=f'X{i+1}')
+        else:
+            for i in range(n):
+                plt.plot(location_list[i], X_list[i], marker=type_, markersize=pch, color=color, label=f'X{i+1}')
     plt.xlabel("Location")
     plt.ylabel("X")
-    plt.legend()
+    if show_legend:
+        plt.legend()
     plt.show()
-
