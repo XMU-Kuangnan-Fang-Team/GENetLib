998f0fe74133ae88c7d00a7f90d4c92ee4742202
Barry57 <160387047+Barry57@users.noreply.github.com>
2024-11-09 11:03:16 +0000

    init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,8 +28,8 @@ exclude_patterns = []
 html_theme = "sphinx_documatt_theme"
 html_static_path = ['_static']
 
-html_logo = '_static/logo.svg'
-html_favicon = '_static/logo.svg'
+html_logo = '_static/logo.png'
+html_favicon = '_static/logo.png'
 
 html_sidebars = {
     'index': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
