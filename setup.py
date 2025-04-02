d45cea93fbc1d8616f1ab3bb9b571e3d40b9e249
Barry <160387047+Barry57@users.noreply.github.com>
2025-04-03 00:14:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='GENetLib',
-    version='1.2.0',
+    version='1.2.1',
     description='A Python Library for Gene–environment Interaction Analysis via Deep Learning',
     author='Yuhao Zhong',
     author_email='Barry57@163.com',
