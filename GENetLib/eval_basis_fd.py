fbc584b9a305cf001ce7be446408b5d541f00cd9
zyh722 <1504356393@qq.com>
2025-04-25 10:24:09 +0800

    Update eval_basis_fd.py
    In the package, I found that lfd can only be a constant, so I wrote the lfd function to create functional data.

--- GENetLib/eval_basis_fd.py
+++ GENetLib/eval_basis_fd.py
@@ -7,6 +7,35 @@ from GENetLib.fd import fd
 
 '''Calculate the value of basis functions and functional objects'''
 
+def lfd(nderiv = 0, bwtlist = None):
+    if not isinstance(nderiv, int):
+        raise ValueError("Order of operator is not numeric.")
+    if nderiv < 0:
+        raise ValueError("Order of operator is negative.")
+    if bwtlist is None:
+        bwtlist = []
+    if bwtlist == None:
+        bwtlist = [None] * nderiv
+        if nderiv > 0:
+            conbasis = create_constant_basis()
+            bwtlist = [fd(0, conbasis) for _ in range(nderiv)]
+    nbwt = len(bwtlist)
+    if nbwt != nderiv and nbwt != nderiv + 1:
+        raise ValueError("The size of bwtlist is inconsistent with nderiv.")
+    if nderiv > 0:
+        rangevec = [0, 1]
+        for j in range(nbwt):
+            bfdj = bwtlist[j]
+            bbasis = bfdj['basis']
+            rangevec = bbasis['rangeval']
+            btype = bbasis['btype']
+            if btype != "const":
+                brange = bbasis['rangeval']
+                if rangevec != brange:
+                    raise ValueError("Ranges are not compatible.")
+    Lfdobj = {'nderiv': nderiv, 'bwtlist': bwtlist}
+    return Lfdobj
+
 # Basis functions
 def eval_basis(evalarg, basisobj, Lfdobj = 0, returnMatrix = False):
     if isinstance(Lfdobj, int):
