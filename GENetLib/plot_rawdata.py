d882b2fa677415223e5363010f57fcbef66b71d2
Jill <Barry57@foxmail.com>
2025-04-27 21:55:23 +0800

    Update plot_rawdata.py

--- GENetLib/plot_rawdata.py
+++ GENetLib/plot_rawdata.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 '''Plot functions fitting from densely measured observations'''
 
-def plot_rawdata(location, X, color=None, pch=4, cex=0.9, show_legend=True):
+def plot_rawdata2(location, X, color=None, pch=4, cex=0.9, show_legend=True):
     n, m = X.shape
     type_ = 'o'
     truelengths = np.sum(~np.isnan(X))
@@ -14,8 +14,12 @@ def plot_rawdata(location, X, color=None, pch=4, cex=0.9, show_legend=True):
         else:
             plt.plot(location, X.T, marker=type_, markersize=pch, color=color, label='X')
     else:
-        location_list = [location[i][~np.isnan(X[i, :])] for i in range(n)]
-        X_list = [X[i, ~np.isnan(X[i, :])] for i in range(n)]
+        location_list = []
+        X_list = []
+        for i in range(n):
+            mask = ~np.isnan(X[i, :])
+            location_list.append(location[mask])
+            X_list.append(X[i, mask])
         if color is None:
             for i in range(n):
                 plt.plot(location_list[i], X_list[i], marker=type_, markersize=pch, label=f'X{i+1}')
