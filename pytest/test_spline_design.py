617810f16d419d7c26f32388e1eca1ee7bd8ada0
Barry <160387047+Barry57@users.noreply.github.com>
2025-04-30 17:13:35 +0800

    Update test_spline_design.py

--- pytest/test_spline_design.py
+++ pytest/test_spline_design.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from scipy.interpolate import BSpline
 import pandas as pd
-from GENetLib.spline_design import spline_design
+from GENetLib.fda_func import spline_design
 
 def test_spline_design_norder_one():
     knots = [0, 1]
