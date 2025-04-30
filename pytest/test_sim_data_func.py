a2c791e22b80b287e7df24669e90b789ebabc257
Barry <160387047+Barry57@users.noreply.github.com>
2025-04-30 17:13:15 +0800

    Update test_sim_data_func.py

--- pytest/test_sim_data_func.py
+++ pytest/test_sim_data_func.py
@@ -1,4 +1,4 @@
-from GENetLib.sim_data_func import sim_data_func
+from GENetLib.sim_data import sim_data_func
 
 def test_sim_data_func():
     func_continuous = sim_data_func(n=1500, m=30, ytype='Continuous', seed=123)
