fe137483677ce7810439901d954971b0754350b6
Kuangnan Fang <xmufkn@xmu.edu.cn>
2025-05-15 00:02:41 +0800

    Update __init__.py

--- GENetLib/__init__.py
+++ GENetLib/__init__.py
@@ -5,7 +5,7 @@ from .fda_func import get_basis_matrix, ppbspline, ycheck, wtcheck, vec2lfd, nor
 from .GE_Net import GE_Net
 from .survival_costfunc_cindex import neg_par_log_likelihood, c_index
 from .fda_func import inprod
-from .fda_func import fd
+from .fda_func import fd, fdpar, fdparcheck
 from .pre_data import pre_data1, pre_data2
 from .sim_data import sim_data_scalar, sim_data_func
 from .fda_func import spline_design
@@ -25,7 +25,7 @@ from .plot_gene import plot_fd, plot_rawdata
 __all__ = ['basis_fd', 'create_bspline_basis','create_expon_basis','create_fourier_basis',
            'create_monomial_basis','create_power_basis','create_constant_basis',
            'bspline_mat','expon_mat','fourier_mat','monomial_mat','polyg_mat','power_mat',
-           'get_basis_matrix','GE_Net','neg_par_log_likelihood','c_index',
+           'get_basis_matrix','GE_Net','neg_par_log_likelihood','c_index', 'fdpar', 'fdparcheck',
            'inprod','fd','pre_data1','pre_data2','sim_data_scalar','sim_data_func',
            'spline_design','fd_chk','knotmultchk','eval_basis','eval_fd','dense_to_func',
            'scalar_l2train','scalar_mcp_l2train','scalar_ge','grid_scalar_ge','func_ge','grid_func_ge',
