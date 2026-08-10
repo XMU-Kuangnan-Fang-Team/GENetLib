bd190ad5933050b06d732af44d5fde040c7c0382
Kuangnan Fang <xmufkn@xmu.edu.cn>
2026-08-10 14:50:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 setup(
     name="GENetLib",
-    version="1.2.8",
+    version="1.2.9",
     description="A Python Library for Gene–environment Interaction Analysis via Deep Learning",
     author="Yuhao Zhong",
     author_email="Barry57@163.com",
